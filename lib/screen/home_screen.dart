4b2897529f9cab6016c3fbc1f8174213218f61ce
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-11 18:33:57 +0900

    family modifier

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_v1/layout/default_layout.dart';
+import 'package:riverpod_v1/screen/family_modifier_screen.dart';
 import 'package:riverpod_v1/screen/future_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_provider_screen.dart';
@@ -45,6 +46,14 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => FamilyModifierScreen()),
+              );
+            },
+            child: Text('FamilyModifierScreen'),
           ),
         ],
       ),

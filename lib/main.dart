9a3f13510255181650d21291ace7fc72bc260208
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-12 23:45:41 +0900

    provider observe

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_v1/riverpod/provider_observe.dart';
 import 'package:riverpod_v1/screen/home_screen.dart';
 
 void main() {
-  runApp(ProviderScope(
-    child: MaterialApp(
-      home: HomeScreen(),
+  runApp(
+    ProviderScope(
+      observers: [
+        Logger(),
+      ],
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
     ),
-  ));
+  );
 }

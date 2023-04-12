46a3d90109a79fbacad0f342ec5c763d3beb86e3
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-12 10:47:29 +0900

    listen provider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_v1/layout/default_layout.dart';
 import 'package:riverpod_v1/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_v1/screen/family_modifier_screen.dart';
 import 'package:riverpod_v1/screen/future_provider_screen.dart';
+import 'package:riverpod_v1/screen/listen_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_provider_screen.dart';
 import 'package:riverpod_v1/screen/stream_provider_screen.dart';
@@ -63,6 +64,14 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => ListenProviderScreen()),
+              );
+            },
+            child: Text('ListenProviderScreen'),
           ),
         ],
       ),

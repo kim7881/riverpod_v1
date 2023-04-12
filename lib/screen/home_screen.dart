065c77b6a23c62c9900fa522550d2c60614b115b
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-12 17:15:41 +0900

    select provider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:riverpod_v1/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_v1/screen/family_modifier_screen.dart';
 import 'package:riverpod_v1/screen/future_provider_screen.dart';
 import 'package:riverpod_v1/screen/listen_provider_screen.dart';
+import 'package:riverpod_v1/screen/select_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_v1/screen/state_provider_screen.dart';
 import 'package:riverpod_v1/screen/stream_provider_screen.dart';
@@ -72,6 +73,14 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('ListenProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => SelectProviderScreen()),
+              );
+            },
+            child: Text('SelectProviderScreen'),
           ),
         ],
       ),
